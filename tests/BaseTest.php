794ea2b7d743b7fd5d112147c6fe0d47c54be231
Max zhang <373884384@qq.com>
2021-09-14 12:04:29 +0800

    忽略测试用例中的配置文件

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -6,9 +6,12 @@ use PHPUnit\Framework\TestCase;
 
 class BaseTest extends TestCase
 {
+    protected $config;
+
     public function __construct()
     {
         parent::__construct();
+        $this->config = include "config.php";
     }
 
 }
---
+++ tests/config.example.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    "HaoDanKu_apikey" => ''
+];
