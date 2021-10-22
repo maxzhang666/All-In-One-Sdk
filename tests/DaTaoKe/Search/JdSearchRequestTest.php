06322e05bc608fce614b57dd5c9d39a521d7fa11
MaxZhang <373884384@qq.com>
2021-10-22 14:58:47 +0800

    京东搜索测试

---
+++ tests/DaTaoKe/Search/JdSearchRequestTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\DaTaoKe\Search;
+
+use MaxZhang\AllInOne\Apps\DaTaoKe\Search\JdSearchRequest;
+use Tests\DaTaoKe\DaTaoKeBaseTest;
+
+class JdSearchRequestTest extends DaTaoKeBaseTest
+{
+
+    /**
+     * @test
+     */
+    function search()
+    {
+        $req         = new JdSearchRequest();
+        $req->skuIds = '10020075095716';
+
+        $res = $this->client->execute($req);
+
+        print_r($res);
+
+        self::assertIsBool(array_key_exists('code', $res));
+    }
+}
